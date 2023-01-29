13c09a0fc20d7e2e986c6c3a385cbf606258210f
Pranav K Nayak <es20btech11035@iith.ac.in>
2023-01-29 13:32:02 +0530

    added support for prerequisite cycles

--- dbms-2/assignment1/ex2.java
+++ dbms-2/assignment1/ex2.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ex2 {
@@ -11,6 +12,7 @@ public class ex2 {
   private final String username = "postgres";
   private final String password = "postgres";
 
+  ArrayList<String> alreadyVisited = new ArrayList<>();
 
   public Connection connect() throws SQLException {
     return DriverManager.getConnection(url, username, password);
@@ -29,6 +31,7 @@ public class ex2 {
   public void displayResultSetRecursive(ResultSet rs, int toplevel) throws SQLException{
     ResultSetMetaData rsmd =rs.getMetaData();
     int maxLength = 20;
+    ArrayList<String> prerequisites = new ArrayList<>();
     String prereq_id;
     while(rs.next()){
       for(int i = 1; i <= rsmd.getColumnCount(); i++){
@@ -37,7 +40,15 @@ public class ex2 {
       }
       System.out.println(" ");
       prereq_id = rs.getString("prereq_id");
-      printPrerequisites(prereq_id, toplevel + 1);
+      // printPrerequisites(prereq_id, toplevel + 1);
+      prerequisites.add(prereq_id);
+    }
+    for(String prq: prerequisites){
+      if(alreadyVisited.contains(prq)){
+        continue;
+      }
+      alreadyVisited.add(prq);
+      printPrerequisites(prq, toplevel + 1);
     }
   }
 
